0f928bb711ac925acf63050de36a358455c5abb3
sahithi5c7 <sahithipadmakar@gmail.com>
2023-11-22 09:54:05 +0530

    first commit

---
+++ csk/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+
+def msd(request):
+    return render(request,'msd.html')
+
+def raina(request):
+    return HttpResponse('<h1>IAM A TEAM PLAYER OF CSK<h1>')
+
+
+
